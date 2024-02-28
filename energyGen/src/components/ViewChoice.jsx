dd3c3f0ad6e6f65bd9ca6099ee4d092829030f6a
Ell534 <elliot94b@gmail.com>
2024-02-28 22:45:23 +0000

    change some styling

--- energyGen/src/components/ViewChoice.jsx
+++ energyGen/src/components/ViewChoice.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function ViewChoice() {
     return (
-        <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto">
+        <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto">
             <p className="uppercase font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">
                 Choose what you want to see, energy generation information or
                 carbon intensity data...
             </p>
-            <button className="px-8 py-4 rounded-md">
+            <button className="px-8 py-2 rounded-2xl border-[3px] border-amber-950 border-solid">
                 <p>Generation</p>
             </button>
-            <button className="px-8 py-4 rounded-md border-[2px] border-amber-950 border-solid bg-slate-600">
+            <button className="px-8 py-2 rounded-2xl border-[3px] border-amber-950 border-solid">
                 <p>Carbon Intensity</p>
             </button>
         </div>
