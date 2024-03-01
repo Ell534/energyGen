2f2e01df13e56b1ca3f0689bd6c0b7e802e0f06c
Ell534 <elliot94b@gmail.com>
2024-03-01 16:50:34 +0000

    add conditional rendering logic with home button to return to view choice page

--- energyGen/src/App.jsx
+++ energyGen/src/App.jsx
@@ -1,15 +1,17 @@
 import Generation from './components/Generation';
 import CarbonIntensity from './components/CarbonIntensity';
 import ViewChoice from './components/ViewChoice';
+import { useState } from 'react';
+
 
 function App() {
 
-
+    const [view, setView] = useState('home')
     return (
         <main className='min-h-screen flex flex-col bg-[#114232] text-[#fcdc2a] text-sm sm:text-base'>
-            <ViewChoice />
-            <Generation />
-            <CarbonIntensity />
+            {view === 'home' && <ViewChoice setView={setView} />}
+            {view === 'generation' && <Generation setView={setView} />}
+            {view === 'carbon' && <CarbonIntensity setView={setView} />}
         </main>
     );
 }
--- energyGen/src/components/ViewChoice.jsx
+++ energyGen/src/components/ViewChoice.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-export default function ViewChoice() {
+export default function ViewChoice({ setView }) {
+
     return (
         <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
             <p className="uppercase font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">
                 Choose what you want to see, energy generation information or
                 carbon intensity data...
             </p>
-            <button className="px-8 py-2 rounded-2xl bg-[#87a922] text-[#114232] greenShadow duration-200">
+            <button onClick={() => setView('generation')} className="px-8 py-2 rounded-2xl bg-[#87a922] text-[#114232] greenShadow duration-200">
                 <p>Generation <i className="fa-solid fa-bolt"></i></p>
             </button>
-            <button className="px-8 py-2 rounded-2xl bg-[#87a922] text-[#114232] greenShadow duration-200">
-                <p>Carbon Intensity <i class="fa-solid fa-smog"></i></p>
+            <button onClick={() => setView('carbon')} className="px-8 py-2 rounded-2xl bg-[#87a922] text-[#114232] greenShadow duration-200">
+                <p>Carbon Intensity <i className="fa-solid fa-smog"></i></p>
             </button>
         </div>
     );
