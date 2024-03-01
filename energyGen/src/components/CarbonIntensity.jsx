5d04e6f7a5885ab3c0685a663c1967362706cae0
Ell534 <elliot94b@gmail.com>
2024-03-01 18:41:27 +0000

    add jest as devdep and move home button above text on generation and carbon intensity components

--- energyGen/src/components/CarbonIntensity.jsx
+++ energyGen/src/components/CarbonIntensity.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function CarbonIntensity({ setView }) {
     return (
         <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto">
-            <p className="uppercase font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">
-                This will display carbon intensity data from the API
-            </p>
             <button onClick={() => setView('home')} className="px-8 py-2 rounded-2xl bg-[#87a922] text-[#114232] greenShadow duration-200">
                 <p>Home <i className="fa-solid fa-bolt"></i></p>
             </button>
+            <p className="uppercase font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">
+                This will display carbon intensity data from the API
+            </p>
         </div>
     );
 }
--- energyGen/src/components/ViewChoice.jsx
+++ energyGen/src/components/ViewChoice.jsx
@@ -5,8 +5,7 @@ export default function ViewChoice({ setView }) {
     return (
         <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
             <p className="uppercase font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">
-                Choose what you want to see, energy generation information or
-                carbon intensity data...
+                Select a Data Option from Below
             </p>
             <button onClick={() => setView('generation')} className="px-8 py-2 rounded-2xl bg-[#87a922] text-[#114232] greenShadow duration-200">
                 <p>Generation <i className="fa-solid fa-bolt"></i></p>
